9cffe19cb44251422283100cd95204e126783eaf
Evgueni Souleimanov <esoule@100500.ca>
2015-05-07 21:08:17 -0400

    undo deletion of target/etckeeper-commit-early.sh

---
+++ target/etckeeper-commit-early.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+if [ ! -x /usr/bin/etckeeper ] ; then
+    exit 0
+fi
+
+# workaround for "fatal: $HOME is not set"
+if [ -z "$HOME" ] ; then
+    export HOME=/root
+fi
+
+cat <<"____EOF" >/etc/.gitignore
+# begin section inserted by centos-livecd-scripts
+/group-
+/gshadow-
+/passwd-
+/shadow
+/shadow-
+/selinux/targeted/policy/
+/selinux/targeted/modules/active/*.pp
+/selinux/targeted/modules/active/commit_num
+/selinux/targeted/modules/active/modules/
+/selinux/targeted/modules/active/policy.kern
+yum.repos.d/tmp-*.repo
+*.bak
+
+
+# end section inserted by centos-livecd-scripts
+#
+
+____EOF
+/sbin/restorecon /etc/.gitignore
+
+/usr/bin/etckeeper init    >/dev/null
+
+/usr/bin/etckeeper commit "Initial commit"    >/dev/null
+
+/usr/bin/etckeeper vcs gc    >/dev/null
+
+exit 0
