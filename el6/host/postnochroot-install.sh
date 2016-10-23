fc48ba96effcca439660afb8cb4548bf90ec947f
Evgueni Souleimanov <esoule@100500.ca>
2016-10-23 15:53:48 -0400

    fix indent

--- el6/host/postnochroot-install.sh
+++ el6/host/postnochroot-install.sh
@@ -13,8 +13,8 @@ cp ${INSTALL_ROOT}${FTDIR}/centos-installed-packages*.txt $LIVE_ROOT/
 # add livecd-iso-to-disk utility on the LiveCD
 # only works on x86, x86_64
 if [ "$(uname -i)" = "i386" -o "$(uname -i)" = "x86_64" ]; then
-  if [ ! -d $LIVE_ROOT/LiveOS ]; then mkdir -p $LIVE_ROOT/LiveOS ; fi
-  cp /usr/bin/livecd-iso-to-disk $LIVE_ROOT/LiveOS
+	if [ ! -d $LIVE_ROOT/LiveOS ]; then mkdir -p $LIVE_ROOT/LiveOS ; fi
+	cp /usr/bin/livecd-iso-to-disk $LIVE_ROOT/LiveOS
 fi
 
 /bin/bash -x ${ROOT_CLS_TOP_DIR}/host/edit-isolinux-cfg.sh
